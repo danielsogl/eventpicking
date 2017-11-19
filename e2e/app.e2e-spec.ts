772ec13653435dd1a0b90f24dcb14786b273dd3b
Daniel <mytechde@outlook.com>
2017-11-19 14:34:58 +0100

    fix e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('dhbw-projekt-app App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getTitle()).toEqual('DhbwProjektApp');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,7 +5,7 @@ export class AppPage {
     return browser.get('/home');
   }
 
-  getParagraphText() {
-    return element(by.css('h1')).getText();
+  getTitle() {
+    return browser.getTitle();
   }
 }
