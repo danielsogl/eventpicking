309025a3729b9c71a14c18527322f5f389533656
Daniel <mytechde@outlook.com>
2017-11-21 20:44:09 +0100

    Auto stash before merge of "develop" and "origin/develop"

---
+++ src/app/services/utility/utility.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+/**
+ * Some utility functions in one service
+ * @author Daniel Sogl
+ */
+@Injectable()
+export class UtilityService {
+
+  constructor() { }
+
+}
