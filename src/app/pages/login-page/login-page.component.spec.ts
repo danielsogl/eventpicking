5fb3e38d2245e44f8af5174bb25b7e02f3e6ccfb
Daniel <mytechde@outlook.com>
2017-11-26 22:39:36 +0100

    fix login unit tests

--- src/app/pages/login-page/login-page.component.spec.ts
+++ src/app/pages/login-page/login-page.component.spec.ts
@@ -1,16 +1,21 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFirestore, AngularFirestoreModule } from 'angularfire2/firestore';
 
+import { FakeLoader } from '../../../../jest-mocks/fake-loader';
 import { environment } from '../../../environments/environment';
+import { FirebaseErrorPipe } from '../../pipes/firebase-error/firebase-error.pipe';
 import { FirebaseAuthService } from '../../services/auth/firebase-auth/firebase-auth.service';
+import { MDBBootstrapModule } from '../../typescripts/free/index';
+import { MDBBootstrapModulePro } from '../../typescripts/pro/index';
 import { LoginPageComponent } from './login-page.component';
-import { ModalModule } from 'angular-bootstrap-md/modals/modal.module';
-import { ModalDirective } from 'angular-bootstrap-md/modals/modal.directive';
 
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
@@ -19,16 +24,23 @@ describe('LoginPageComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        BrowserDynamicTestingModule,
+        NoopAnimationsModule,
+        MDBBootstrapModule.forRoot(),
+        MDBBootstrapModulePro.forRoot(),
         RouterTestingModule,
+        FormsModule,
         AngularFireModule.initializeApp(environment.firebase),
-        MDBBootstrapModule.forRoot()
+        TranslateModule.forRoot({
+          loader: {provide: TranslateLoader, useClass: FakeLoader}
+        })
       ],
       providers: [
         FirebaseAuthService,
         AngularFireAuth,
         { provide: AngularFirestore, depends: AngularFirestoreModule }
       ],
-      declarations: [LoginPageComponent],
+      declarations: [LoginPageComponent, FirebaseErrorPipe],
       schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
