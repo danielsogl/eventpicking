68d30435ffbf73b748ca47eca421e892ba4ad2d8
Daniel <mytechde@outlook.com>
2017-12-03 17:07:08 +0100

    fix travis

--- src/app/pages/photographer-page/photographer-page.component.spec.ts
+++ src/app/pages/photographer-page/photographer-page.component.spec.ts
@@ -4,23 +4,40 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 import { FakeLoader } from '../../../../jest-mocks/fake-loader';
 import { PhotographerPageComponent } from './photographer-page.component';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../../../environments/environment';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { FirebaseAuthService } from '../../services/auth/firebase-auth/firebase-auth.service';
+import { FirebaseFirestoreService } from '../../services/firebase/firestore/firebase-firestore.service';
+import {
+  AngularFirestore,
+  AngularFirestoreModule
+} from 'angularfire2/firestore';
 
 describe('PhotographerPageComponent', () => {
   let component: PhotographerPageComponent;
   let fixture: ComponentFixture<PhotographerPageComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        TranslateModule.forRoot({
-          loader: {provide: TranslateLoader, useClass: FakeLoader},
-        })
-      ],
-      declarations: [ PhotographerPageComponent ]
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          RouterTestingModule,
+          AngularFireModule.initializeApp(environment.firebase),
+          TranslateModule.forRoot({
+            loader: { provide: TranslateLoader, useClass: FakeLoader }
+          })
+        ],
+        providers: [
+          AngularFireAuth,
+          FirebaseAuthService,
+          FirebaseFirestoreService,
+          { provide: AngularFirestore, depends: AngularFirestoreModule }
+        ],
+        declarations: [PhotographerPageComponent]
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PhotographerPageComponent);
