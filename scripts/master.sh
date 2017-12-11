9f7d71fe3420608211717d24e880e69676960d79
Daniel <mytechde@outlook.com>
2017-12-11 13:33:38 +0100

    wip travis

--- scripts/master.sh
+++ scripts/master.sh
@@ -1,2 +1,4 @@
+echo "Build PWA"
 npm run pwa
+echo "Deploy to firebase"
 firebase deploy --only hosting -P master --token $FIREBASE_TOKEN
--- scripts/staging.sh
+++ scripts/staging.sh
@@ -1,2 +1,4 @@
+echo "Build application"
 ng build --prod --env=dev
+echo "Deploy to firebase"
 firebase deploy --only hosting -P staging --token $FIREBASE_TOKEN
