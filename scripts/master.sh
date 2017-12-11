530666de9e3186526bc902398635a6dd4b0ca376
Daniel <mytechde@outlook.com>
2017-12-11 14:45:55 +0100

    WIP travis

--- scripts/master.sh
+++
@@ -1,7 +0,0 @@
-echo -e "\n#2 = Building project..."
-npm run pwa
-
-echo -e "\n#3 - Deploying to Firebase Hosting..."
-firebase deploy --only hosting -P prod --token $FIREBASE_TOKEN
-
-echo -e "\nDone. The project deployed successfully."
--- scripts/staging.sh
+++
@@ -1,7 +0,0 @@
-echo -e "\n#2 = Building project..."
-ng build --prod --env=dev
-
-echo -e "\n#3 - Deploying to Firebase Hosting..."
-firebase deploy --only hosting -P staging --token $FIREBASE_TOKEN
-
-echo -e "\nDone. The project deployed successfully."
