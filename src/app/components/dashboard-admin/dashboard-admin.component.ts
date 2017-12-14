04ffdc90b351e7ffcafa185ccc9671af099ded28
Daniel <mytechde@outlook.com>
2017-12-14 11:17:44 +0100

    wip

--- src/app/components/dashboard-admin/dashboard-admin.component.ts
+++ src/app/components/dashboard-admin/dashboard-admin.component.ts
@@ -4,6 +4,9 @@ import { Log } from 'ng2-logger';
 import { User } from '../../classes/user';
 import { FirebaseAuthService } from '../../services/auth/firebase-auth/firebase-auth.service';
 import { FirebaseFirestoreService } from '../../services/firebase/firestore/firebase-firestore.service';
+import { Observable } from 'rxjs/Observable';
+import { Event } from '../../classes/event';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard-admin',
@@ -15,9 +18,17 @@ export class DashboardAdminComponent implements OnInit {
 
   public user: User;
 
+  public userForm: FormGroup;
+  public eventForm: FormGroup;
+  public printingHouseForm: FormGroup;
+
+  public users: Observable<User[]>;
+  public events: Observable<Event[]>;
+
   constructor(
     private auth: FirebaseAuthService,
-    private afs: FirebaseFirestoreService
+    private afs: FirebaseFirestoreService,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit() {
@@ -28,5 +39,10 @@ export class DashboardAdminComponent implements OnInit {
       this.user = user;
       this.log.d('Loaded user', user);
     });
+
+    // Load users from Firestore
+    this.users = this.afs.getAllUser().valueChanges();
+    // Load events from Firestore
+    this.events = this.afs.getAllEvents().valueChanges();
   }
 }
