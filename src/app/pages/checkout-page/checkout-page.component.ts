3fc1d7e1a43396a9c727ec3cb0edfa88e5460ab9
Unknown <t.kriessler@gmail.com>
2017-12-29 16:25:33 +0100

    Stepper Checkout-Page
    Added stepper to checkout-page to navigate through different templates. Focused template is marked as active.

--- src/app/pages/checkout-page/checkout-page.component.ts
+++ src/app/pages/checkout-page/checkout-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { Log } from 'ng2-logger';
 
 @Component({
@@ -9,10 +9,49 @@ import { Log } from 'ng2-logger';
 export class CheckoutPageComponent implements OnInit {
   private log = Log.create('CheckoutPageComponent');
 
+  public contactDetailsStatus: string;
+  public paymentDeliveryStatus: string;
+  public checkOrderStatus: string;
+
+  public template: TemplateRef<any>;
+  @ViewChild('loadingTmpl') loadingTmpl: TemplateRef<any>;
+  @ViewChild('checkoutContactDetails') checkoutContactDetails: TemplateRef<any>;
+  @ViewChild('checkoutPaymentDelivery')
+  checkoutPaymentDelivery: TemplateRef<any>;
+  @ViewChild('checkoutCheckOrder') checkoutCheckOrder: TemplateRef<any>;
+
   constructor() {}
 
   ngOnInit() {
     this.log.color = 'orange';
     this.log.d('Component initialized');
+    this.setTemplate('contactDetails');
+  }
+
+  setTemplate(template: string) {
+    switch (template) {
+      case 'contactDetails':
+        this.template = this.checkoutContactDetails;
+        this.contactDetailsStatus = 'active';
+        this.paymentDeliveryStatus = '';
+        this.checkOrderStatus = '';
+        break;
+      case 'paymentDelivery':
+        this.template = this.checkoutPaymentDelivery;
+        this.contactDetailsStatus = '';
+        this.paymentDeliveryStatus = 'active';
+        this.checkOrderStatus = '';
+        break;
+      case 'checkOrder':
+        this.template = this.checkoutCheckOrder;
+        this.contactDetailsStatus = '';
+        this.paymentDeliveryStatus = '';
+        this.checkOrderStatus = 'active';
+        break;
+
+      default:
+        this.template = this.loadingTmpl;
+        break;
+    }
   }
 }
