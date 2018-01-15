70fc616534fef5c31a7a1ce734f2effabf0ac00d
Daniel <mytechde@outlook.com>
2018-01-15 13:55:56 +0100

    wip

--- functions/clean-up.js
+++ functions/clean-up.js
@@ -54,16 +54,66 @@ exports.deleteImageHandler = event => {
     });
 };
 
-exports.deleateUserFromDBHandler = event => {};
+exports.deleateUserFromDBHandler = event => {
+  const userID = event.params.userID;
 
-exports.deleteuserFromFirebaseHandler = event => {
+  // Delete events
+
+  // Delete user from firebase
   return admin
     .auth()
     .deleteUser(userID)
     .then(function() {
-      console.log("Successfully deleted user");
-    })
-    .catch(function(error) {
-      console.log("Error deleting user:", error);
-    });
+      console.log('Successfully deleted user');
+    })
+    .catch(function(error) {
+      console.log('Error deleting user:', error);
+    });
 };
+
+exports.deleteuserFromFirebaseHandler = event => {
+  const userID = event.params.userID;
+};
+
+function deleteCollection(db, collectionPath, batchSize) {
+  var collectionRef = db.collection(collectionPath);
+  var query = collectionRef.orderBy('__name__').limit(batchSize);
+
+  return new Promise((resolve, reject) => {
+    deleteQueryBatch(db, query, batchSize, resolve, reject);
+  });
+}
+
+function deleteQueryBatch(db, query, batchSize, resolve, reject) {
+  query
+    .get()
+    .then(snapshot => {
+      // When there are no documents left, we are done
+      if (snapshot.size == 0) {
+        return 0;
+      }
+
+      // Delete documents in a batch
+      var batch = db.batch();
+      snapshot.docs.forEach(doc => {
+        batch.delete(doc.ref);
+      });
+
+      return batch.commit().then(() => {
+        return snapshot.size;
+      });
+    })
+    .then(numDeleted => {
+      if (numDeleted === 0) {
+        resolve();
+        return;
+      }
+
+      // Recurse on the next process tick, to avoid
+      // exploding the stack.
+      process.nextTick(() => {
+        deleteQueryBatch(db, query, batchSize, resolve, reject);
+      });
+    })
+    .catch(reject);
+}
