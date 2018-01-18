03a8537396d2583c5b50a9dc7714545b3ce1651a
Unknown <t.kriessler@gmail.com>
2018-01-18 22:34:07 +0100

    Functionality added
    -Search Event by name
    -Show all events that names start with entered value

--- src/app/pages/event-search-page/event-search-page.component.ts
+++ src/app/pages/event-search-page/event-search-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Log } from 'ng2-logger';
 
+import { Event } from '../../classes/event';
 import { FirebaseFirestoreService } from '../../services/firebase/firestore/firebase-firestore.service';
 
 /**
@@ -15,6 +16,8 @@ import { FirebaseFirestoreService } from '../../services/firebase/firestore/fire
 export class EventSearchPageComponent implements OnInit {
   /** Logger */
   private log = Log.create('EventSearchPageComponent');
+  public events: Event[] = [];
+  public searchedEvents: Event[] = [];
 
   /**
    * Constructor
@@ -28,5 +31,28 @@ export class EventSearchPageComponent implements OnInit {
   ngOnInit() {
     this.log.color = 'orange';
     this.log.d('Component initialized');
+    this.afs
+      .getAllEvents()
+      .valueChanges()
+      .subscribe(events => {
+        this.events = events;
+        this.log.info('Events: ', this.events);
+      });
+  }
+
+  /** Input key listeners */
+  onKey(event: any) {
+    // clear searchedEvents
+    this.searchedEvents.splice(0);
+    // if input value = '' all events would be shown
+    if (event.target.value !== '') {
+      for (let i = 0; i < this.events.length; i++) {
+        if (this.events[i].name.startsWith(event.target.value)) {
+          if (!this.searchedEvents.includes(this.events[i])) {
+            this.searchedEvents.push(this.events[i]);
+          }
+        }
+      }
+    }
   }
 }
