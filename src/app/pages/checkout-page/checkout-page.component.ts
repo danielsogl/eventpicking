e79e07edda1611cda11ee1e23c0cfc3a5c0fcb8b
Daniel <mytechde@outlook.com>
2018-01-23 19:50:56 +0100

    finished user dashboard

--- src/app/pages/checkout-page/checkout-page.component.ts
+++ src/app/pages/checkout-page/checkout-page.component.ts
@@ -111,7 +111,7 @@ export class CheckoutPageComponent implements OnInit {
               }
               this.paypalItems.push({
                 currency: CURRENCY.EUR,
-                description: 'Ein Bild',
+                description: JSON.stringify(this.cartItems[i].info),
                 name: `${this.cartItems[i].eventname}/${
                   this.cartItems[i].name
                 }`,
